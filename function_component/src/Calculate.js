3177b9702753ef1fb9504fca58102ddce86dd857
yoo16 <yohei.yoshikawa@gmail.com>
2021-02-07 00:25:10 +0900

    UC

--- function_component/src/Calculate.js
+++ function_component/src/Calculate.js
@@ -1,10 +1,11 @@
-import React, { useState, useRef } from 'react'
+import React, { useState } from 'react'
 import './Message.css'
 
-export const Calculate = () => {
-    const tax = 1.1;
+export function Calculate() {
     const [count, setCount] = useState(0)
     const [show, setShow] = useState(false);
+
+    const tax = 1.1
     const initialValues = {
         price: 0,
         totalPrice: 0,
@@ -21,37 +22,34 @@ export const Calculate = () => {
     }
 
     function handleInit(e) {
+        setCount(0);
         setValues(initialValues);
     }
 
     return (
         <div>
-            <h2>Counter</h2>
+            <h3>Counter</h3>
             <p>You clicked {count} times</p>
             <button onClick={() => setCount(count + 1)}>Click</button>
-
-            <h2>Show/Hide</h2>
             <div>
                 {show ?
                     <div className="message">
                         <p>Hello!</p>
                         <button onClick={() => setShow(false)}>Hide</button>
                     </div>
-                    : ''}
+                    : ''
+                }
                 <button onClick={() => setShow(true)}>Show</button>
+            </div>
 
-                <div>
-                    <h3>Total Price</h3>
-                    <p>{values.totalPrice}</p>
-                    <input type="text"
-                        value={values.price}
-                        onChange={handleCalculate}
-                    />
-                </div>
-
-                <div>
-                    <button onClick={handleInit}>Clear</button>
-                </div>
+            <div>
+                <h3>Total Price</h3>
+                <p>{values.totalPrice}</p>
+                <input type="text"
+                    value={values.price}
+                    onChange={handleCalculate}
+                />
+                <button onClick={handleInit}>Clear</button>
             </div>
         </div>
     )
