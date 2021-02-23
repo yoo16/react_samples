f1ef9ab3e787e9feb8b83bc6293f92fdaae74522
yoo16 <yohei.yoshikawa@gmail.com>
2021-02-23 14:34:29 +0900

    add typescript + react

--- next_type/components/ListItem.tsx
+++ next_type/components/ListItem.tsx
@@ -4,15 +4,15 @@ import Link from 'next/link'
 import { User } from '../interfaces'
 
 type Props = {
-  data: User
+    data: User
 }
 
 const ListItem = ({ data }: Props) => (
-  <Link href="/users/[id]" as={`/users/${data.id}`}>
-    <a>
-      {data.id}: {data.name}
-    </a>
-  </Link>
+    <Link href="/users/[id]" as={`/users/${data.id}`}>
+        <a>
+            {data.id}: {data.name}
+        </a>
+    </Link>
 )
 
 export default ListItem
---
+++ next_type/pages/users/index.tsx
@@ -0,0 +1,40 @@
+import { GetStaticProps } from 'next'
+import Link from 'next/link'
+
+import { User } from '../../interfaces'
+// import { sampleUserData } from '../../utils/sample-data'
+import Layout from '../../components/Layout'
+import List from '../../components/List'
+
+//const apiURL = 'http://localhost/api/user/get';
+const apiURL = 'http://localhost:8000/api/user/';
+
+type Props = {
+    items: User[]
+}
+
+const WithStaticProps = ({ items }: Props) => (
+    <Layout title="Users List | Next.js + TypeScript Example">
+        <h1>Users List</h1>
+        <p>
+            Example fetching data from inside <code>getStaticProps()</code>.
+        </p>
+        <p>You are currently on: /users</p>
+        <List items={items} />
+        <p>
+            <Link href="/">
+                <a>Go home</a>
+            </Link>
+        </p>
+    </Layout>
+)
+
+export const getStaticProps: GetStaticProps = async () => {
+    const url = `${apiURL}get`
+    const res = await fetch(url)
+    const json = await res.json()
+    const items: User[] = json
+    return { props: { items } }
+}
+
+export default WithStaticProps
